16cfe8865a07b61df71fa076c1513acdb9392b46
DamyanKolev <damyankolev2@gmail.com>
2024-01-26 16:56:27 +0200

    add new util

---
+++ reactapp/src/utils/auth.ts
@@ -0,0 +1,18 @@
+export async function isJWTTokenValid(): Promise<boolean> {
+    const token = localStorage.getItem("token")
+
+    if(token != null) {
+        const response = await fetch("/backend/api/auth//verify-token",{
+            method: "POST",
+            headers: { 
+                "Content-Type": "application/json" 
+            },
+            body: JSON.stringify(token),
+        })
+
+        if(response.ok) {
+            return await response.json()
+        }
+    }
+    return false
+}

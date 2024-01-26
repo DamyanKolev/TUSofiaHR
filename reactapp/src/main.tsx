7e8e9aa6956353fc98554879c905dd2b9c7a93a7
DamyanKolev <damyankolev2@gmail.com>
2024-01-26 15:48:38 +0200

    Update main tsx

--- reactapp/src/main.tsx
+++ reactapp/src/main.tsx
@@ -6,11 +6,18 @@ import './index.css'
 import "@ui5/webcomponents/dist/Assets.js";
 import "@ui5/webcomponents-fiori/dist/Assets.js";
 import { setTheme } from "@ui5/webcomponents-base/dist/config/Theme.js";
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store.ts';
 setTheme("sap_horizon");
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
-    <ThemeProvider>
-        <App />
-    </ThemeProvider>
+    <Provider store={store}>
+        <ThemeProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ThemeProvider>
+    </Provider>
 );

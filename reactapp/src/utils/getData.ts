ff4280cb6314bc10d8d6df9d83c698002eba62cf
DamyanKolev <damyankolev2@gmail.com>
2024-01-26 16:13:46 +0200

    add new file with functions

---
+++ reactapp/src/utils/getData.ts
@@ -0,0 +1,46 @@
+import { setNullValuesToEmtyString } from "./forms/setNullValuesToEmtyString";
+
+
+export async function getUpdateData<T extends object, D>(data: D, postURL: string): Promise<T | null> {
+    const token = localStorage.getItem("token")
+    const response = await fetch(postURL, {
+        method: "POST",
+        headers: { 
+            "Content-Type": "application/json",
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify(data),
+    })
+    const json = await response.json()
+
+    if (response.ok) {
+        const data = setNullValuesToEmtyString<T>(json.data)
+        return data
+    }
+    else {
+        console.error(json.message)
+        return null
+    }
+}
+
+
+
+export async function getData<T>(postURL: string): Promise<T | null> {
+    const token = localStorage.getItem("token")
+    const response = await fetch(postURL, {
+        method: "GET",
+        headers: { 
+            "Content-Type": "application/json",
+            'Authorization': `Bearer ${token}`
+        },
+    })
+    const json = await response.json()
+
+    if (response.ok) {
+        return json.data
+    }
+    else {
+        console.error(json.message)
+        return null
+    }
+}

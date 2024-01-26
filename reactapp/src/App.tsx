c6b4648f744e76f7fa35a4ac128f297870e0a09a
DamyanKolev <damyankolev2@gmail.com>
2024-01-26 15:48:32 +0200

    Update app file

--- reactapp/src/App.tsx
+++ reactapp/src/App.tsx
@@ -1,11 +1,8 @@
 // import './App.css'
 import { FC, useEffect } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
+import { Route, Routes } from 'react-router-dom';
 import Layout from './layouts/Layout';
-import { Article62, ContractPage, DepartmentPage, InitPage, EmployeePage, EmployeeReports, Home, Login, PositionPage, SchedulePage, Settings } from './pages/PagesBundle';
-import References from './pages/references/References';
+import { Article62, ContractPage, DepartmentPage, EmployeePage, EmployeeReports, Home, Login, PositionPage, References, SchedulePage, Settings } from './pages/PagesBundle';
 import setSelectedTheme from '@utils/themesUtils';
 
 const App: FC = () => {
@@ -15,26 +12,21 @@ const App: FC = () => {
     }, []);
 
     return (
-        <Provider store={store}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Layout />}>
-                        <Route index element={<Home />} />
-                        <Route path="hr" element={<EmployeeReports />} />
-                        <Route path="settings" element={<Settings />} />
-                        <Route path="hr/employee" element={<EmployeePage />} />
-                        <Route path="hr/contract" element={<ContractPage />} />
-                        <Route path="hr/position" element={<PositionPage />} />
-                        <Route path="hr/department" element={<DepartmentPage />} />
-                        <Route path="hr/references" element={<References />} />
-                        <Route path="hr/schedule" element={<SchedulePage />} />
-                        <Route path="hr/references/article62" element={<Article62 />} />
-                    </Route>
-                    <Route path="auth/login" element={<Login />} />
-                    <Route path="init" element={<InitPage />} />
-                </Routes>
-            </BrowserRouter>
-        </Provider>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="hr" element={<EmployeeReports />} />
+                <Route path="settings" element={<Settings />} />
+                <Route path="hr/employee" element={<EmployeePage />} />
+                <Route path="hr/contract" element={<ContractPage />} />
+                <Route path="hr/position" element={<PositionPage />} />
+                <Route path="hr/department" element={<DepartmentPage />} />
+                <Route path="hr/references" element={<References />} />
+                <Route path="hr/schedule" element={<SchedulePage />} />
+                <Route path="hr/references/article62" element={<Article62 />} />
+            </Route>
+                 <Route path="auth/login" element={<Login />} />
+        </Routes>
     );
 }
 

c3b40ee8700b572f58bbed2e0a3d2d71f839edc1
DamyanKolev <damyankolev2@gmail.com>
2024-03-30 16:04:54 +0200

    add new component menu

---
+++ reactapp/src/components/Manus/HRProductSwitch.tsx
@@ -0,0 +1,56 @@
+import { ProductSwitch, ProductSwitchItem } from "@ui5/webcomponents-react";
+import { FC } from "react";
+
+
+
+const HRProductSwitch: FC = () => {
+
+    return (
+        <ProductSwitch>
+            <ProductSwitchItem
+                icon="employee"
+                targetSrc="/hr/employee"
+                titleText="Служители"
+                subtitleText="Служители"
+            />
+
+            <ProductSwitchItem
+                icon="BusinessSuiteInAppSymbols/icon-contract"
+                targetSrc="/hr/contract"
+                titleText="Договори"
+                subtitleText="Договори"
+            />
+
+            <ProductSwitchItem
+                icon="insurance-life"
+                targetSrc="/hr/insurance"
+                titleText="Осигуровки"
+                subtitleText="Осигуровки"
+            />
+
+            <ProductSwitchItem
+                icon="company-view"
+                targetSrc="/hr/department"
+                titleText="Отдели"
+                subtitleText="Отдели"
+            />
+
+            <ProductSwitchItem
+                icon="suitcase"
+                targetSrc="/hr/position"
+                titleText="Позиции"
+                subtitleText="Позиции"
+            />
+
+            <ProductSwitchItem
+                icon="detail-view"
+                targetSrc="/hr/references"
+                titleText="Справки"
+                subtitleText="Справки"
+            />
+        </ProductSwitch>
+    )
+}
+
+
+export default HRProductSwitch

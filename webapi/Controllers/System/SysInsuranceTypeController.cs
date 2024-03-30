ebab6bad3f9823f3eefb3a4d0a496c703439a3fa
DamyanKolev <damyankolev2@gmail.com>
2024-03-30 15:53:12 +0200

    add new controllers

---
+++ webapi/Controllers/System/SysInsuranceTypeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapi.Models;
+using webapi.Services.System;
+
+namespace webapi.Controllers.System
+{
+    //[Authorize(Roles = IdentityRoles.Admin)]
+    //[Authorize(Roles = IdentityRoles.Accountant)]
+    [ApiController]
+    public class SysInsuranceTypeController : ControllerBase
+    {
+        private readonly ISysInsuranceTypesService _insuranceTypesService;
+
+        public SysInsuranceTypeController(ISysInsuranceTypesService insuranceTypesService)
+        {
+            _insuranceTypesService = insuranceTypesService;
+        }
+
+        [HttpPost("/api/sys/insurance-type/all", Name = "GetAllInsuranceType")]
+        public IActionResult GetInsuranceType()
+        {
+            var result = _insuranceTypesService.SelectAll();
+
+            return Ok(result.Response);
+        }
+    }
+}
---
+++ webapi/Controllers/System/SysPaymentTypeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapi.Services.System;
+
+namespace webapi.Controllers.System
+{
+    //[Authorize(Roles = IdentityRoles.Admin)]
+    //[Authorize(Roles = IdentityRoles.Accountant)]
+    [ApiController]
+    public class SysPaymentTypeController: ControllerBase
+    {
+        private readonly ISysPaymentTypeService _paymentTypeService;
+
+        public SysPaymentTypeController(ISysPaymentTypeService paymentTypeService)
+        {
+            _paymentTypeService = paymentTypeService;
+        }
+
+        [HttpPost("/api/sys/insurance-type/all", Name = "GetAllInsuranceType")]
+        public IActionResult GetInsuranceType()
+        {
+            var result = _paymentTypeService.SelectAll();
+
+            return Ok(result.Response);
+        }
+    }
+}

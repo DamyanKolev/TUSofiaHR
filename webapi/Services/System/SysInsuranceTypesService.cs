0f15ff16107eaeba00a490e1c7c2e6ea45c3332f
DamyanKolev <damyankolev2@gmail.com>
2024-03-30 15:53:38 +0200

    add new system controllers

---
+++ webapi/Services/System/SysInsuranceTypesService.cs
@@ -0,0 +1,30 @@
+﻿using webapi.Models.System;
+using webapi.Models;
+using System.Net;
+using webapi.Constants;
+
+namespace webapi.Services.System
+{
+    public interface ISysInsuranceTypesService
+    {
+        public ResponseWithStatus<DataResponse<List<SysInsuranceType>>> SelectAll();
+    }
+    public class SysInsuranceTypesService : ISysInsuranceTypesService
+    {
+        public readonly DatabaseContext _context;
+
+        public SysInsuranceTypesService(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public ResponseWithStatus<DataResponse<List<SysInsuranceType>>> SelectAll()
+        {
+            var insuranceTypes = _context.SysInsuranceTypes
+                .OrderBy(p => p.Id)
+                .ToList();
+
+            return ResponseBuilder.CreateDataResponseWithStatus(HttpStatusCode.OK, MessageConstants.MESSAGE_SUCCESS_SELECT, insuranceTypes);
+        }
+    }
+}
---
+++ webapi/Services/System/SysPaymentTypeService.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using webapi.Constants;
+using webapi.Models.System;
+using webapi.Models;
+
+namespace webapi.Services.System
+{
+    public interface ISysPaymentTypeService
+    {
+        public ResponseWithStatus<DataResponse<List<SysPaymentType>>> SelectAll();
+    }
+    public class SysPaymentTypeService : ISysPaymentTypeService
+    {
+        public readonly DatabaseContext _context;
+
+        public SysPaymentTypeService(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public ResponseWithStatus<DataResponse<List<SysPaymentType>>> SelectAll()
+        {
+            var paymentTypes = _context.SysPaymentTypes
+                .OrderBy(p => p.Id)
+                .ToList();
+
+            return ResponseBuilder.CreateDataResponseWithStatus(HttpStatusCode.OK, MessageConstants.MESSAGE_SUCCESS_SELECT, paymentTypes);
+        }
+    }
+}

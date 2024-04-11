3fed7434805b781936958b06e2d34f88ef09e9e2
DamyanKolev <damyankolev2@gmail.com>
2024-04-11 16:23:37 +0300

    add swager endpoint

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -94,6 +94,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
 app.UseHttpsRedirection();
 
 app.UseAuthentication();

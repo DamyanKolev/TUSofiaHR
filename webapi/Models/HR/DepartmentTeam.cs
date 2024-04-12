135d62d50cf7b2323d538a00e84496403423ab33
DamyanKolev <damyankolev2@gmail.com>
2024-04-12 16:48:59 +0300

    Add new model DepartmentTeam

---
+++ webapi/Models/HR/DepartmentTeam.cs
@@ -0,0 +1,20 @@
+﻿namespace webapi.Models.HR
+{
+    public class DepartmentTeam
+    {
+        public int Id { get; set; }
+        public required string TeamName { get; set; }
+        public int DepartmentId { get; set; }
+        public Department? Department { get; set; }
+        public Nullable<int> ManagerId { get; set; }
+        public Employee? Manager { get; set; }
+    }
+
+
+    public record DepartmentTeamInsert
+    {
+        public required string TeamName { get; set; }
+        public int DepartmentId { get; set; }
+        public Nullable<int> ManagerId { get; set; }
+    }
+}

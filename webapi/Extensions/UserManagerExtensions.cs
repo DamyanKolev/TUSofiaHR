d97a9296a96ae16bdb7953e4411dc06ddfabc8ed
DamyanKolev <damyankolev2@gmail.com>
2024-04-12 16:14:47 +0300

    add new extention class

---
+++ webapi/Extensions/UserManagerExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using webapi.Models.Auth;
+
+namespace webapi.Extensions
+{
+    public static class UserManagerExtensions
+    {
+        public async static Task AddUpdateClaim(this UserManager<User> userManager, User user, Claim newClaim)
+        {
+            var claims = await userManager.GetClaimsAsync(user);
+            var claim = claims.Where(c => c.Type == newClaim.Type).FirstOrDefault();
+
+            if (claim != null)
+            {
+                await userManager.RemoveClaimAsync(user, claim);
+            }
+            await userManager.AddClaimAsync(user, newClaim);
+        }
+    }
+}

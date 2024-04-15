d3964291b625879235362fd4254e5dad082ad0c8
DamyanKolev <damyankolev2@gmail.com>
2024-04-15 15:46:30 +0300

    add new controllers

---
+++ webapi/Controllers/HR/DepartmentTeamController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using webapi.Models.HR;
+using webapi.Models;
+using webapi.Services.HR;
+using Microsoft.AspNetCore.Authorization;
+
+namespace webapi.Controllers.HR
+{
+    [Authorize]
+    [ApiController]
+    public class DepartmentTeamController : ControllerBase
+    {
+        private readonly IDepartmentTeamService _departmentTeamService;
+
+        public DepartmentTeamController(IDepartmentTeamService departmentTeamService)
+        {
+            _departmentTeamService = departmentTeamService;
+        }
+
+
+        [HttpPost("/api/department-team/create", Name = "CreateDepartmentTeam")]
+        public IActionResult CreateDepartment([FromBody] DepartmentTeamInsert departmentTeamInsert)
+        {
+            var result = _departmentTeamService.CreateDepartmentTeam(departmentTeamInsert);
+
+            if (result.StatusCode.Equals(HttpStatusCode.BadRequest))
+                return BadRequest(result.Response);
+
+            return Ok(result.Response);
+        }
+
+
+        [HttpPut("/api/department-team/update", Name = "UpdateDepartemntTeam")]
+        public IActionResult UpdateDepartment([FromBody] DepartmentTeam departmentTeam)
+        {
+            var result = _departmentTeamService.UpdateDepartmentTeam(departmentTeam);
+
+            if (result.StatusCode.Equals(HttpStatusCode.NotFound))
+                return NotFound(result.Response);
+            else if (result.StatusCode.Equals(HttpStatusCode.BadRequest))
+                return BadRequest(result.Response);
+
+            return Ok(result.Response);
+        }
+
+
+        [HttpDelete("/api/department-team/delete", Name = "DeleteDepartmentTeam")]
+        public IActionResult DeleteDepartment([FromBody] int departmentTeamId)
+        {
+            var result = _departmentTeamService.DeleteDepartmentTeam(departmentTeamId);
+
+            if (result.StatusCode.Equals(HttpStatusCode.NotFound))
+                return NotFound(result.Response);
+            else if (result.StatusCode.Equals(HttpStatusCode.BadRequest))
+                return BadRequest(result.Response);
+
+            return Ok(result.Response);
+        }
+
+
+        [HttpPost("/api/department-team/page", Name = "GetDepartmentTeamsPage")]
+        public IActionResult GetDepartmentsPage([FromBody] PageInfo pageInfo)
+        {
+            var result = _departmentTeamService.GetDepartmentTeamsPage(pageInfo);
+
+            return Ok(result.Response);
+        }
+
+
+        [HttpGet("/api/department-team/all", Name = "GetAllDepartmentTeams")]
+        public IActionResult GetAllDepartments()
+        {
+            var result = _departmentTeamService.GetAllDepartmentTeams();
+
+            return Ok(result.Response);
+        }
+    }
+}
---
+++ webapi/Controllers/HR/InitController.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using webapi.Models;
+using webapi.Services.HR;
+
+namespace webapi.Controllers.HR
+{
+    [Authorize]
+    [ApiController]
+    public class InitController : ControllerBase
+    {
+        private readonly IInitService _initService;
+
+        public InitController(IInitService initService)
+        {
+            _initService = initService;
+        }
+
+        [HttpPost("/api/init-hr", Name = "InitHR")]
+        public IActionResult InitHR([FromBody] InitAppData initData)
+        {
+            var result = _initService.InitHR(initData);
+
+            if (result.StatusCode.Equals(HttpStatusCode.BadRequest))
+                return BadRequest(result.Response);
+
+            return Ok(result.Response);
+        }
+
+
+        [HttpGet("/api/is-init", Name = "IsInit")]
+        public IActionResult IsInit()
+        {
+            var result = _initService.IsInit();
+
+            if (result.StatusCode.Equals(HttpStatusCode.BadRequest))
+                return BadRequest(result.Response);
+
+            return Ok(result.Response);
+        }
+    }
+}

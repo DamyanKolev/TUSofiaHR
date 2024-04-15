67bb056cafd944c82506b2f97a2daeeae4e094f1
DamyanKolev <damyankolev2@gmail.com>
2024-04-15 08:47:46 +0300

    add new fields and new models

--- reactapp/src/models/HR/Departmnet.ts
+++ reactapp/src/models/HR/Departmnet.ts
@@ -1,17 +1,42 @@
+import { Optional } from "@/types/Optional";
+import { DepartmentView } from "../TableViews/DepartmentView";
+
 export interface Department {
     id: int;
     departmentName: string,
+    description: Optional<string>
+    managerId: Optional<string>
 }
 
 export interface DepartmentDTO {
     departmentName: string,
+    description: Optional<string>
+    managerId: Optional<string>
 }
 
 export const defaultDepartmentDTO: DepartmentDTO = {
-    departmentName: ""
+    departmentName: "",
+    description: null,
+    managerId: null
 }
 
 export const defaultDepartmentUpdateDTO: Department = {
     id: 0,
-    departmentName: ""
+    departmentName: "",
+    description: null,
+    managerId: null
+}
+
+export interface DepartmentUpdateData {
+    managerName: Optional<string>
+}
+
+export const defaultDepartmentUpdateData: DepartmentUpdateData = {
+    managerName: "",
+}
+
+export function createDepartmentUpdateData(departmentView: DepartmentView): DepartmentUpdateData {
+    return {
+        managerName: departmentView.managerName,
+    }
 }

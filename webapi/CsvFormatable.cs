d8e58241e70ca2d32815a90a445ed4ab3cabc903
DamyanKolev <damyankolev2@gmail.com>
2024-04-15 15:47:03 +0300

    add new abstract class, which convert filds value into csv string

---
+++ webapi/CsvFormatable.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace webapi
+{
+    public abstract class CsvFormatable
+    {
+        public string ToCsvString()
+        {
+            // fields type
+            Type type = GetType();
+            FieldInfo[] fields = type.GetFields();
+
+            // Building a CSV string
+            StringBuilder csvBuilder = new StringBuilder();
+            foreach (FieldInfo field in fields)
+            {
+                var value = field.GetValue(this);
+                if (value != null)
+                {
+                    csvBuilder.Append($"{value},");
+                }
+                else
+                {
+                    csvBuilder.Append($",");
+                }
+            }
+
+            // Remove last comma
+            csvBuilder.Length -= 1;
+
+            return csvBuilder.ToString();
+        }
+    }
+}

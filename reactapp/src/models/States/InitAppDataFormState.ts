e7d6c3246b4b328ae2d90860505c1a150a65511c
DamyanKolev <damyankolev2@gmail.com>
2024-04-15 11:43:24 +0300

    change names

--- reactapp/src/models/States/InitAppDataFormState.ts
+++ reactapp/src/models/States/InitAppDataFormState.ts
@@ -2,14 +2,13 @@ import { DepartmentFormState, defaultDepartmentInsertFormState } from "./departm
 import { defaultDepTeamInsertFormState, DepartmentTeamFormState } from "./departmentTeam/DepartmentTeamFormState";
 import { PositionFormState, defualtPositionInsertFormState } from "./position/PositionFormState";
 
-export interface PositionDepartmentState {
+export interface InitAppDataFormState {
     positionInsert: PositionFormState,
     departmentInsert: DepartmentFormState
     departmentTeamInsert: DepartmentTeamFormState
-
 }
 
-export const defaultPositionDepartmentState: PositionDepartmentState = {
+export const defaultInitAppDataFormState: InitAppDataFormState = {
     positionInsert: defualtPositionInsertFormState,
     departmentInsert: defaultDepartmentInsertFormState,
     departmentTeamInsert: defaultDepTeamInsertFormState 

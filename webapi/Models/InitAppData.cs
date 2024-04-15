f564631c209c550e73828e81a4a710f078d82b32
DamyanKolev <damyankolev2@gmail.com>
2024-04-15 15:46:07 +0300

    add new model

---
+++ webapi/Models/InitAppData.cs
@@ -0,0 +1,11 @@
+﻿using webapi.Models.HR;
+
+namespace webapi.Models
+{
+    public record InitAppData
+    {
+        public required PositionDTO PositionInsert {  get; set; }
+        public required DepartmentDTO DepartmentInsert { get; set; }
+        public required DepartmentTeamInsert DepartmentTeamInsert { get; set; }
+    }
+}

11203ad2db6d1867dc1706721757b2dad7346cef
DamyanKolev <damyankolev2@gmail.com>
2024-04-15 08:47:21 +0300

    add new slice to store

--- reactapp/src/store/store.ts
+++ reactapp/src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import toggleSlice from './slices/toggleSlice';
 import formToggleSlice from './slices/formToggleSlice';
+import loginSlice from './slices/loginSlice';
 
 export const store =  configureStore({
     reducer: {
         isSuccess: toggleSlice,
-        isSuccessForm: formToggleSlice        
+        isSuccessForm: formToggleSlice,     
+        isLoggedIn: loginSlice
     }
 })
 

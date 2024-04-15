f77cce3a4535456b605870be3a6a924ca8b1500a
DamyanKolev <damyankolev2@gmail.com>
2024-04-15 08:13:34 +0300

    add new join info model

---
+++ reactapp/src/models/JoinTableInfo/DepTeamJoinTablesInfo.ts
@@ -0,0 +1,25 @@
+import { JoinTableInfo } from "./JoinTableInfo";
+
+
+export interface DepTeamJoinTablesInfo {
+    departmentId: JoinTableInfo,
+    managerId: JoinTableInfo,
+}
+
+
+export const depTeamJoinTableInfo:DepTeamJoinTablesInfo = {
+    departmentId: {
+        filterField: "departmentName",
+        description: "departmentName",
+        contentFields: ["departmentName"],
+        headerText: "Служители",
+        tableURL: "/api/hr/department/all"
+    },
+    managerId: {
+        filterField: "employeeName",
+        description: "companyEmployeeId",
+        contentFields: ["employeeName"],
+        headerText: "Служители",
+        tableURL: "/api/hr/employee/page"
+    },
+}

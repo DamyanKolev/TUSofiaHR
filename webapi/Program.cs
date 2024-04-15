89022a73aecbb4e1225d3eabff8eeb4b050b46da
DamyanKolev <damyankolev2@gmail.com>
2024-04-15 11:45:01 +0300

    remove unused using

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using webapi;
-using webapi.Controllers.System;
 using webapi.Models.Auth;
 using webapi.Services.Auth;
 using webapi.Services.HR;

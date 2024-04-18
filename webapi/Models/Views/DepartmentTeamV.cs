ba8501fe82058cb8a9e6a6dec134424fee29c2ea
DamyanKolev <damyankolev2@gmail.com>
2024-04-18 16:33:00 +0300

    add new view models

---
+++ webapi/Models/Views/DepartmentTeamV.cs
@@ -0,0 +1,10 @@
+﻿namespace webapi.Models.Views
+{
+    public class DepartmentTeamV
+    {
+        public int Id { get; set; }
+        public required string TeamName { get; set; }
+        public string? ManagerName { get; set; }
+        public required string ddepartmentName { get; set; }
+    }
+}
---
+++ webapi/Models/Views/DepartmentV.cs
@@ -0,0 +1,10 @@
+﻿namespace webapi.Models.Views
+{
+    public class DepartmentV
+    {
+        public int Id { get; set; }
+        public required string DepartmentName { get; set; }
+        public string? ManagerName { get; set; }
+        public string? Description { get; set; }
+    }
+}

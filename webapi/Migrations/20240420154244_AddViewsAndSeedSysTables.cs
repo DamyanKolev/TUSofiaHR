9db9d032c4374a71f8921446cf5e668899437dcc
DamyanKolev <damyankolev2@gmail.com>
2024-04-20 19:11:33 +0300

    add migrations

---
+++ webapi/Migrations/20240420154244_AddViewsAndSeedSysTables.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddViewsAndSeedSysTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            const string baseDir = "Resources\\SQLScripts\\";
+            var sqlFile = Path.Combine(baseDir, "Init_Up.sql");
+            migrationBuilder.Sql(File.ReadAllText(sqlFile));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            const string baseDir = "Resources\\SQLScripts\\";
+            var sqlFile = Path.Combine(baseDir, "Init_Down.sql");
+            migrationBuilder.Sql(File.ReadAllText(sqlFile));
+        }
+    }
+}
---
+++ webapi/Migrations/20240420160625_SeedHRTables.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedHRTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            const string baseDir = "Resources\\SQLScripts\\";
+            var sqlFile = Path.Combine(baseDir, "Seed_Data_Up.sql");
+            migrationBuilder.Sql(File.ReadAllText(sqlFile));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

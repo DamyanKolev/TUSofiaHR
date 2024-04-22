5b6192cb985c6b896bb57eb080935068c1b61740
DamyanKolev <damyankolev2@gmail.com>
2024-04-22 11:40:39 +0300

    remove unused file

--- reactapp/src/utils/formaters.ts
+++
@@ -1,7 +0,0 @@
-export function formatDate(date: Date): string {
-    const year: number = date.getFullYear();
-    const month: string = String(date.getMonth() + 1).padStart(2, "0");
-    const day: string = String(date.getDate()).padStart(2, "0");
-
-    return `${year}-${month}-${day}`;
-}

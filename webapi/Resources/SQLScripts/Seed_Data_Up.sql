998e1d69e3bef3cf2c91cf7f0190b31f0d5e1589
DamyanKolev <damyankolev2@gmail.com>
2024-04-22 11:40:52 +0300

    fix seed hr tables sql script

--- webapi/Resources/SQLScripts/Seed_Data_Up.sql
+++ webapi/Resources/SQLScripts/Seed_Data_Up.sql
@@ -1,37 +1,37 @@
-INSERT INTO addresses (id, street_address, postal_code, district, municipilaty, region, populated_place)
+INSERT INTO addresses (street_address, postal_code, district, municipilaty, region, populated_place)
 VALUES 
-	(1, 'ул. Тодор Каблешков',2022, 'Гоце Делчев', 'Столична', 'София', 'София'),
-	(2, 'ул. Петър Горанов',2022, 'Люлин', 'Столична', 'София', 'София');;
+	('ул. Тодор Каблешков',2022, 'Гоце Делчев', 'Столична', 'София', 'София'),
+	('ул. Петър Горанов',2022, 'Люлин', 'Столична', 'София', 'София');
 
 
 
-INSERT INTO departments (id, department_name)
+INSERT INTO departments (department_name)
 VALUES 
-	(1, 'Web Application');
+	('Web Application');
 
 
-INSERT INTO department_teams (id, team_name, department_id)
+INSERT INTO department_teams (team_name, department_id)
 VALUES 
-	(1, 'Frontend', 1),
-	(2, 'Backend', 1);
+	('Frontend', 1),
+	('Backend', 1);
 
 
-INSERT INTO personal_data (id, identity_text, identity_code, address_id)
+INSERT INTO personal_data (identity_text, identity_code, address_id)
 VALUES 
-	(1, '0055234122', 0, 1),
-	(2, '0055244112', 0, 2);
+	('c', 0, 1),
+	('0055244112', 0, 2);
 
 
-INSERT INTO insurances (id, doo_withouth_tzpb_insurer, doo_withouth_tzpb_employee, health_insurance, health_insurance_article40, health_insurance_insurer, health_insurance_employee, teacher_pension_fund, professional_pension_fund, universal_pension_insurer, universal_pension_employee, insurance_type_id)
+INSERT INTO insurances (doo_withouth_tzpb_insurer, doo_withouth_tzpb_employee, health_insurance, health_insurance_article40, health_insurance_insurer, health_insurance_employee, teacher_pension_fund, professional_pension_fund, universal_pension_insurer, universal_pension_employee, insurance_type_id)
 VALUES 
-	(1, '14.80', '0.00', '8.00', '8.00', '8.00', '0.00', '0.00', '12.00', '5.00', '0.00', 4),
-	(2, '14.80', '0.00', '8.00', '8.00', '8.00', '0.00', '0.00', '12.00', '5.00', '0.00', 4);
+	('14.80', '0.00', '8.00', '8.00', '8.00', '0.00', '0.00', '12.00', '5.00', '0.00', 4),
+	('14.80', '0.00', '8.00', '8.00', '8.00', '0.00', '0.00', '12.00', '5.00', '0.00', 4);
 
 
-INSERT INTO positions (id, position_name, min_salary, max_salary)
+INSERT INTO positions (position_name, min_salary, max_salary)
 VALUES 
-	(1, 'Junior Frontend Dev', '1500', '2000'),
-	(2, 'Junior Backend Dev', '1300', '1800');
+	('Junior Frontend Dev', '1500', '2000'),
+	('Junior Backend Dev', '1300', '1800');
 
 
 INSERT INTO employees (first_name, middle_name, surname, email, phone_number, company_employee_id, personal_data_id, department_team_id, position_id, insurance_id)

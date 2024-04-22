640b5f2197f55cc549c84a0efb971555462d8c95
DamyanKolev <damyankolev2@gmail.com>
2024-04-22 11:43:17 +0300

    remove unused code

--- reactapp/src/pages/references/Article62.tsx
+++ reactapp/src/pages/references/Article62.tsx
@@ -1,8 +1,5 @@
-import PageBar from "@components/Bars/PageBar"
 import SmartTable from "@components/Table/SmartTable"
 import article62Columns from "@models/TableColumns/Article62Columns"
-import { Button, ButtonDesign } from "@ui5/webcomponents-react"
-import { downloadFile } from "@utils/download"
 import { FC } from "react"
 
 
@@ -11,12 +8,6 @@ const Article62: FC = () => {
     const tableTitle = "Уведомление по Чл. 62"
     const tableURL = "/api/hr/references/article62"
 
-    const onClick = async () =>{
-        const fetchURL = "/api/hr/references/article62/declaration"
-        const filename = "article62"
-        downloadFile(fetchURL, filename)
-    }
-
     return (
         <div className="flexible-columns ui5-content-density-compact">
             <SmartTable
@@ -24,11 +15,6 @@ const Article62: FC = () => {
                 columns={article62Columns}
                 tableURL={tableURL}
                 title={tableTitle}
-                header={
-                    <PageBar title={tableTitle}>
-                        <Button design={ButtonDesign.Transparent} onClick={onClick} slot="endContent">Справка</Button>
-                    </PageBar>
-                }
             />
         </div>
     )

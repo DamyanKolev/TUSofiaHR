347c23a10be3d00468928297ff4136a9fc516004
DamyanKolev <damyankolev2@gmail.com>
2024-04-29 08:30:19 +0300

    add new field UserId

--- webapi/Models/HR/Company.cs
+++ webapi/Models/HR/Company.cs
@@ -1,10 +1,14 @@
-﻿namespace webapi.Models.HR
+﻿using webapi.Models.Auth;
+
+namespace webapi.Models.HR
 {
     public record Company
     {
         public int Id { get; set; }
         public required string CompanyName { get; set; }
         public required string CompanyEic { get; set; }
+        public required int UserId { get; set; }
+        public User? User { get; set;}
     }
 
 
@@ -12,5 +16,6 @@
     {
         public required string CompanyName { get; set; }
         public required string CompanyEic { get; set; }
+        public required int UserId { get; set; }
     }
 }

dfc106fd7a0f0709bdd2c63277dfd2aa117b394d
DamyanKolev <damyankolev2@gmail.com>
2024-04-29 08:40:58 +0300

    remove unused field

--- reactapp/src/models/States/InitAppDataFormState.ts
+++ reactapp/src/models/States/InitAppDataFormState.ts
@@ -1,15 +1,12 @@
 import { DepartmentFormState, defaultDepartmentInsertFormState } from "./department/DepartmentFormState";
-import { defaultDepTeamInsertFormState, DepartmentTeamFormState } from "./departmentTeam/DepartmentTeamFormState";
 import { PositionFormState, defualtPositionInsertFormState } from "./position/PositionFormState";
 
 export interface InitAppDataFormState {
     positionInsert: PositionFormState,
     departmentInsert: DepartmentFormState
-    departmentTeamInsert: DepartmentTeamFormState
 }
 
 export const defaultInitAppDataFormState: InitAppDataFormState = {
     positionInsert: defualtPositionInsertFormState,
     departmentInsert: defaultDepartmentInsertFormState,
-    departmentTeamInsert: defaultDepTeamInsertFormState 
 }

e28f59e97b9002033cb7329c2f8ffcad236a714d
DamyanKolev <damyankolev2@gmail.com>
2024-04-29 08:21:44 +0300

    add new auth model

---
+++ webapi/Models/Auth/UserData.cs
@@ -0,0 +1,10 @@
+﻿using webapi.Models.HR;
+
+namespace webapi.Models.Auth
+{
+    public class UserData
+    {
+        public required Company Company { get; set; }
+        public required AuthTokens Tokens { get; set; }
+    }
+}

93305d89fb7db096227012219ae84837f9dc063c
DamyanKolev <damyankolev2@gmail.com>
2024-04-29 08:27:50 +0300

    add two new field ManagerId and ParentId

--- webapi/Models/Views/DepartmentV.cs
+++ webapi/Models/Views/DepartmentV.cs
@@ -7,5 +7,7 @@
         public string? ManagerName { get; set; }
         public string? Description { get; set; }
         public string? ParentDepartmentName { get; set; }
+        public Nullable<int> ManagerId { get; set; }
+        public Nullable<int> ParentId {get; set;}
     }
 }

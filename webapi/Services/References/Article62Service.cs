385eda1dfb12a701950da4a280a69af0a36e6e7e
DamyanKolev <damyankolev2@gmail.com>
2024-05-06 16:59:16 +0300

    remove GetArticle62Page method and two new methods for creating csv and pdf file

--- webapi/Services/References/Article62Service.cs
+++ webapi/Services/References/Article62Service.cs
@@ -1,5 +1,9 @@
 ﻿using System.Net;
+using System.Text;
 using AutoMapper;
+using HandlebarsDotNet;
+using Newtonsoft.Json.Linq;
+using PuppeteerSharp;
 using webapi.Constants;
 using webapi.Models;
 using webapi.Models.Views;
@@ -8,7 +12,8 @@ namespace webapi.Services.References
 {
     public interface IArticle62Service
     {
-        public ResponseWithStatus<DataResponse<PageResponse<Article62V>>> GetArticle62Page(PageInfo pageInfo);
+        public Task<byte[]> GetArticle62PDF();
+        public byte[] GetArticle62CSV();
     }
     public class Article62Service : IArticle62Service
     {
@@ -23,19 +28,77 @@ namespace webapi.Services.References
 
 
 
-        public ResponseWithStatus<DataResponse<PageResponse<Article62V>>> GetArticle62Page(PageInfo pageInfo) {
-            var articles = _context.ContractV
-                .Select(c => _mapper.Map<Article62V>(c))
-                .Skip((pageInfo.PageNumber - 1) * pageInfo.PageSize)
-                .Take(pageInfo.PageSize)
+        public async Task<byte[]> GetArticle62PDF()
+        {
+            var articles = _context.Article62V
+                .Select(c => JObject.Parse(c.JsonObject))
                 .ToList();
-            
-            var countRecords = _context.EmployeeV.ToList().Count;
-            var pages = (int) Math.Ceiling(Decimal.Divide(countRecords, pageInfo.PageSize));
-            PageResponse<Article62V> pageResponse = new (pages, countRecords, articles);
+            //Initialize HTML to PDF converter. 
 
-            return ResponseBuilder.CreateDataResponseWithStatus(HttpStatusCode.OK, MessageConstants.MESSAGE_SUCCESS_SELECT, pageResponse);
+
+            var path = Path.Combine("Resources/Templates", "reference-article62.hbs");
+            if (File.Exists(path))
+            {
+                string hbsTemplate = File.ReadAllText(path);
+                var template = Handlebars.Compile(hbsTemplate);
+                DateTime today = DateTime.Today;
+
+                var data = new
+                {
+                    articles,
+                    current_date = today
+                };
+                var result = template(data);
+                Console.WriteLine(result);
+
+
+
+                using var browserFetcher = new BrowserFetcher();
+                await browserFetcher.DownloadAsync();
+
+                using (var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+                {
+                    Headless = true,
+                }))
+                {
+                    using (var page = await browser.NewPageAsync())
+                    {
+                        var pdfOptions = new PuppeteerSharp.PdfOptions();
+                        pdfOptions.Landscape = true;
+
+
+                        await page.SetContentAsync(result);
+                        byte[] pdfBytes = await page.PdfDataAsync(pdfOptions);
+                        return pdfBytes;
+                    }
+                }
+            }
+            return Encoding.ASCII.GetBytes("");
         }
+
+
+        public byte[] GetArticle62CSV()
+        {
+            var articles = _context.Article62V
+                .Select(c => c.CsvString)
+                .ToList();
+
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var streamWriter = new StreamWriter(memoryStream, Encoding.UTF8))
+                {
+                    foreach (var csvString in articles)
+                    {
+                        streamWriter.WriteLine(csvString);
+                    }
+                }
+
+                byte[] csvData = memoryStream.ToArray();
+
+                return csvData;
+            }
+        }
+
 
     }
 }

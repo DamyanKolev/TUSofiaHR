30bf0eeacfa0bd0a4b62e366fbeaa1d13f087f80
DamyanKolev <damyankolev2@gmail.com>
2024-05-06 16:54:42 +0300

    remove unused model

--- reactapp/src/models/BionaField.ts
+++
@@ -1,9 +0,0 @@
-export default interface BionaField {
-    name: string,
-    field_type: string,
-    display_name: string,
-    include_options: Array<string>,
-    exclude_options: Array<string>,
-    filterable: boolean,
-    sortable: boolean,
-}

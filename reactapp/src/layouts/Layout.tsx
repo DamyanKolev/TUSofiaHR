1424297a3c4f5a42cf2348ef44dbb3708104f3a8
DamyanKolev <damyankolev2@gmail.com>
2024-05-06 17:11:14 +0300

    fix layout style

--- reactapp/src/layouts/Layout.tsx
+++ reactapp/src/layouts/Layout.tsx
@@ -1,37 +1,30 @@
-import { FC, useState } from 'react';
+import { FC, Fragment, useState } from 'react';
 import './Layout.css';
 import { Outlet } from 'react-router-dom';
 import '@ui5/webcomponents-icons/dist/menu';
-import ShellBarMenu from '../components/Bars/ShellBarMenu';
-import NavBar from '../components/Bars/NavBar';
+import ShellBarMenu from '@components/Bars/ShellBarMenu';
+import NavBar from '@components/Bars/NavBar';
 import { FlexBox } from '@ui5/webcomponents-react';
 
 
 const Layout: FC = () => {
     const [collapsed, setCollapsed] = useState<boolean>(true);
-    const [width, setWidth] = useState<string>('calc(100vw - 5.21rem)');
 
     const hideShowSideNav = () => {
         setCollapsed(!collapsed);
-        if (collapsed) {
-            setWidth('calc(100vw - 17.71rem)')
-        }
-        else {
-            setWidth('calc(100vw - 5.21rem)')
-        }
     };
 
     return (
-        <>
+        <Fragment key={'app.container'}>
             <ShellBarMenu hideShowSideNav={hideShowSideNav} />
 
-            <FlexBox style={{height:"calc(100vh - 3.25rem)"}}>
+            <FlexBox style={{height:"calc(100vh - 3.73rem)", marginBlockStart: ".5rem", gap: ".5rem"}}>
                 <NavBar collapsed={collapsed} />
-                <div className="content-container" style={{width:`${width}`}}>
+                <div className='content-container'>
                     <Outlet/> 
                 </div>  
             </FlexBox>
-        </>
+        </Fragment>
     );
 };
 

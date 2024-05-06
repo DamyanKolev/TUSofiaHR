e0ce692411e3f36ef0a1d18276699f55c0d1177d
DamyanKolev <damyankolev2@gmail.com>
2024-05-06 17:10:03 +0300

    update logic, change ProductSwitchItem to button and add new function onClickArticle

--- reactapp/src/pages/references/References.tsx
+++ reactapp/src/pages/references/References.tsx
@@ -1,35 +1,64 @@
-import { FC, Fragment } from 'react';
-import { ProductSwitch, ProductSwitchItem } from "@ui5/webcomponents-react";
+import { FC, useState } from 'react';
+import { FlexBox, FlexBoxDirection, ProductSwitch, ProductSwitchItem, ProductSwitchItemDomRef, Ui5CustomEvent } from "@ui5/webcomponents-react";
 import "@ui5/webcomponents-icons-business-suite/contract"
 import PageBar from '@components/Bars/PageBar';
+import { createPortal } from 'react-dom';
+import ReferenceDialog from '@/components/Dialogs/ReferenceDialog';
 
 const References: FC = () => {
+    const [referenceData, setReferenceData] = useState<string>("")
+    const [open, setOpen] = useState<boolean>(false)
+
+    const onClickArticle = (event: Ui5CustomEvent<ProductSwitchItemDomRef, never>) => {
+        const currRefData = event.target.dataset.table
+        if (currRefData) {
+            setReferenceData(currRefData)
+            setOpen(true)
+        }
+    }
+
     return (
-        <Fragment>
+        <FlexBox direction={FlexBoxDirection.Column}>
             <PageBar title='Справки'/>
             <ProductSwitch>
                 <ProductSwitchItem
+                    onClick={onClickArticle}
+                    data-table="article62"
                     icon="BusinessSuiteInAppSymbols/icon-contract"
-                    targetSrc="/hr/references/article62"
                     titleText="Уведомление"
                     subtitleText="Член 62 от закона за ТД"
                 />
 
                 <ProductSwitchItem
+                    onClick={onClickArticle}
+                    data-table="declaration1"
                     icon="BusinessSuiteInAppSymbols/icon-contract"
-                    targetSrc="/hr/references/article123"
-                    titleText="Уведомление"
-                    subtitleText="Член 123 от закона за ТД"
+                    titleText="Декларация 1"
+                    subtitleText="Декларация 1"
                 />
 
                 <ProductSwitchItem
+                    onClick={onClickArticle}
+                    data-table="declaration6"
                     icon="BusinessSuiteInAppSymbols/icon-contract"
-                    targetSrc="/hr/references/declaration"
-                    titleText="Декларации"
-                    subtitleText="Декларации обр. 1,3 и 6"
+                    titleText="Декларация 6"
+                    subtitleText="Декларация 6"
                 />
             </ProductSwitch>
-        </Fragment>
+
+
+            {
+                open &&
+                createPortal(
+                    <ReferenceDialog
+                        open={open}
+                        setOpen={setOpen}
+                        referenceData={referenceData}
+                    />,
+                    document.body
+                )
+            }
+        </FlexBox>
     )
 }
 

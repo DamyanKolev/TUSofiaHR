45483dbd03e98add18cfed4833bb9423c207ee05
DamyanKolev <damyankolev2@gmail.com>
2024-05-06 17:07:37 +0300

    fix donwloadFile and remove downloadFile2

--- reactapp/src/utils/download.ts
+++ reactapp/src/utils/download.ts
@@ -1,4 +1,4 @@
-export async function downloadFile(fetchURL:string, filename: string) {
+export async function downloadFile(fetchURL:string, filename: string, fileType: string) {
     const token = sessionStorage.getItem("accessToken")
     const response = await fetch(fetchURL, {
         method: "GET",
@@ -11,33 +11,34 @@ export async function downloadFile(fetchURL:string, filename: string) {
     if (response.ok) {
         const blob = await response.blob()
         //Create a Blob from the PDF Stream
-        const file = new Blob([blob], { type: "application/pdf" });
+        // const file = new Blob([blob], { type: "application/pdf" });
+        const file = new Blob([blob], { type: fileType });
         //Build a URL from the file
         const fileURL = URL.createObjectURL(file);
         //Open the URL on new Window
-        const pdfWindow = window.open();
-        if(pdfWindow) {
-            pdfWindow.location.href = fileURL;
-        }
-        else(
-            console.error("Window is null")
-        )
-        console.log(filename)
-
-        // const link = document.createElement('a');
-        // link.href = fileURL;
-        // link.download = filename
-        // // Append to html link element page
-        // document.body.appendChild(link);
-        // // Start download
-        // link.click();
-        // // Clean up and remove the link
-        // if(link.parentElement != null) {
-        //     link.parentElement.removeChild(link);
+        // const pdfWindow = window.open();
+        // if(pdfWindow) {
+        //     pdfWindow.location.href = fileURL;
         // }
         // else(
-        //     console.error("link does not have a parent")
+        //     console.error("Window is null")
         // )
+        // console.log(filename)
+
+        const link = document.createElement('a');
+        link.href = fileURL;
+        link.download = filename
+        // Append to html link element page
+        document.body.appendChild(link);
+        // Start download
+        link.click();
+        // Clean up and remove the link
+        if(link.parentElement != null) {
+            link.parentElement.removeChild(link);
+        }
+        else(
+            console.error("link does not have a parent")
+        )
     }
     else {
         console.error(await response.json())
@@ -45,52 +46,3 @@ export async function downloadFile(fetchURL:string, filename: string) {
 }
 
 
-
-
-
-export async function downloadFile2<T>(fetchURL:string, filename: string, bodyData: T) {
-    const token = sessionStorage.getItem("accessToken")
-    const response = await fetch(fetchURL, {
-        method: "POST",
-        body: JSON.stringify(bodyData),
-        headers: { 
-            "Content-Type": "application/json",
-            'Authorization': `Bearer ${token}`
-        },
-    })
-
-    if (response.ok) {
-        const blob = await response.blob()
-        //Create a Blob from the PDF Stream
-        const file = new Blob([blob], { type: "application/pdf" });
-        //Build a URL from the file
-        const fileURL = URL.createObjectURL(file);
-        //Open the URL on new Window
-        const pdfWindow = window.open();
-        if(pdfWindow) {
-            pdfWindow.location.href = fileURL;
-        }
-        else(
-            console.error("Window is null")
-        )
-        console.log(filename)
-
-        // const link = document.createElement('a');
-        // link.href = fileURL;
-        // link.download = filename
-        // // Append to html link element page
-        // document.body.appendChild(link);
-        // // Start download
-        // link.click();
-        // // Clean up and remove the link
-        // if(link.parentElement != null) {
-        //     link.parentElement.removeChild(link);
-        // }
-        // else(
-        //     console.error("link does not have a parent")
-        // )
-    }
-    else {
-        console.error(await response.json())
-    }
-}

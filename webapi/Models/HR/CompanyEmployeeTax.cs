4593ae5e62d4994212afc56d32ace658b3e0d8dc
DamyanKolev <damyankolev2@gmail.com>
2024-05-06 17:03:15 +0300

    add new fields

--- webapi/Models/HR/CompanyEmployeeTax.cs
+++ webapi/Models/HR/CompanyEmployeeTax.cs
@@ -1,9 +1,22 @@
-﻿namespace webapi.Models.HR
+﻿using webapi.Models.System;
+
+namespace webapi.Models.HR
 {
     public record CompanyEmployeeTax
     {
+        public int Id { get; set; }
         public int EmployeeId { get; set; }
-        public Int16 PaymentTypeCode { get; set; }
+        public Employee? Employee { get; set; }
+        public int SysPaymentTypeId { get; set; }
+        public SysPaymentType? SysPaymentType { get; set; }
+        public DateOnly DisbursementAccrualDate { get; set; }
+        public DateOnly CreationDate { get; set; }
+    }
+
+    public class CompanyEmployeeTaxDTO
+    {
+        public int EmployeeId { get; set; }
+        public int SysPaymentTypeId { get; set; }
         public DateOnly DisbursementAccrualDate { get; set; }
     }
 }

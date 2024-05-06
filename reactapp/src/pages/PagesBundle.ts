515b05c700416ad3bcdebf758bf127b0e516d21e
DamyanKolev <damyankolev2@gmail.com>
2024-05-06 17:11:40 +0300

    remove unused pages and import pages as dynamic import

--- reactapp/src/pages/PagesBundle.ts
+++ reactapp/src/pages/PagesBundle.ts
@@ -1,19 +1,18 @@
-import EmployeePage from './hr/EmployeePage'
-import Home from './Home'
-import Login from './auth/Login'
-import ContractPage from './hr/ContractPage'
-import PositionPage from './hr/PositionPage'
-import DepartmentPage from './hr/DepartmentPage'
-import Article62 from './references/Article62'
-import Settings from './SettingsPage'
-import References from './references/References'
-import Article123Page from './references/Article123Page'
-import DeclarationPage from './references/DeclarationPage'
-import InsurancePage from './hr/EmployeeIncomePage'
+
+import { lazy } from 'react'
 
 
+const Home = lazy(() => import('./Home'));
+const EmployeePage = lazy (() => import('./hr/EmployeePage'))
+const Login = lazy (() => import('./auth/Login'))
+const ContractPage = lazy (() => import('./hr/ContractPage'))
+const PositionPage = lazy (() => import('./hr/PositionPage'))
+const DepartmentPage = lazy (() => import('./hr/DepartmentPage'))
+const Settings = lazy (() => import('./SettingsPage'))
+const References = lazy (() => import('./references/References'))
+const EndMonthPage = lazy (() => import('./hr/EndMonthPage'))
 
 export {
-    EmployeePage, Home, Login, ContractPage, PositionPage, DepartmentPage, Article62, Settings, References, Article123Page, 
-    DeclarationPage, InsurancePage
+    EmployeePage, Home, Login, ContractPage, PositionPage, DepartmentPage, Settings, References, 
+    EndMonthPage
 }

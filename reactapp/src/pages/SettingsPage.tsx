bde1a718c3f11e50c7ee77b25437e1c0b9ceaa42
DamyanKolev <damyankolev2@gmail.com>
2024-05-06 17:11:04 +0300

    fix setting page and add save button

--- reactapp/src/pages/SettingsPage.tsx
+++ reactapp/src/pages/SettingsPage.tsx
@@ -1,9 +1,13 @@
 import { Theme, themes } from "@models/Themes"
 import { 
-    Option, FlexBox, FlexBoxAlignItems, FlexBoxDirection, FlexBoxJustifyContent, Page, Select, Title, TitleLevel, Ui5CustomEvent, SelectDomRef 
+    Option, FlexBox, FlexBoxAlignItems, FlexBoxDirection, FlexBoxJustifyContent, Page, Select, Title, TitleLevel, Ui5CustomEvent, SelectDomRef, 
+    Bar,
+    Button,
+    BarDesign,
+    ButtonDesign
 } from "@ui5/webcomponents-react"
 import { SelectChangeEventDetail } from "@ui5/webcomponents/dist/Select.js"
-import { CSSProperties, FC} from "react"
+import { CSSProperties, FC, useState} from "react"
 import { setTheme, getTheme } from "@ui5/webcomponents-base/dist/config/Theme.js";
 import CompanySelect from "@components/Selects/CompanySelect";
 
@@ -14,30 +18,43 @@ const divSeparator: CSSProperties= {
 }
 
 
-const Settings: FC = () => {
+const SettingsPage: FC = () => {
+    const [selectedTheme, setSelectedTheme] = useState<string>("")
+    const [disable, setDisabled] = useState<boolean>(true)
     const storageTheme = localStorage.getItem("theme")
     const currentTheme = storageTheme? storageTheme : getTheme()
+
+    const onSave = () => {
+        if (selectedTheme) {
+            setTheme(selectedTheme)
+            window.localStorage.setItem('theme', selectedTheme);
+            setDisabled(true)
+        }
+    }
 
     const onChange = (event: Ui5CustomEvent<SelectDomRef, SelectChangeEventDetail>) => {
         const selectedOption = event.detail.selectedOption
         const theme = selectedOption.dataset.theme
         if (theme) {
+            setSelectedTheme(theme)
             setTheme(theme)
-            window.localStorage.setItem('theme',theme);
+            if (disable) {setDisabled(false)}
         }
     }
 
 
     return (
         <Page 
-            style={{backgroundColor: "white", height: "calc(100vh - 3.73rem)"}}
-            // footer={<Bar
-            //     design={BarDesign.Footer}
-            //     endContent={<Button design={ButtonDesign.Emphasized}>Save</Button>}
-            // />}
+            style={{backgroundColor: "var(--sapPageHeader_Background)"}}
+            footer={
+                <Bar
+                    design={BarDesign.Footer}
+                    endContent={<Button design={ButtonDesign.Emphasized} onClick={onSave} disabled={disable}>Запази</Button>}
+                />
+            }
         >
             <FlexBox direction={FlexBoxDirection.Column}>
-                <div style={{...divSeparator, margin: "0 0 1rem 0"}}></div>
+            <div style={{...divSeparator, margin: "0 0 1rem 0"}}></div>
                 <FlexBox style={{padding:"0 3rem"}} alignItems={FlexBoxAlignItems.Center} justifyContent={FlexBoxJustifyContent.SpaceBetween}> 
                     <Title level={TitleLevel.H4}>Избор на тема</Title>
                     <Select onChange={onChange} style={{width: "15rem"}}>
@@ -61,4 +78,4 @@ const Settings: FC = () => {
     )
 }
 
-export default Settings
+export default SettingsPage

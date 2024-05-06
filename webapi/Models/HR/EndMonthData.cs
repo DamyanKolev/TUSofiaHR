cec27f000e1c7620580fa9dc25f04b97c199badc
DamyanKolev <damyankolev2@gmail.com>
2024-05-06 17:01:33 +0300

    rename model and add new field CompanyEmployeeTax

--- webapi/Models/HR/EndMonthData.cs
+++ webapi/Models/HR/EndMonthData.cs
@@ -1,13 +1,15 @@
 ﻿namespace webapi.Models.HR
 {
-    public record ScheduleIncomeInsert
+    public record EndMonthDataInsert
     {
         public required IncomeDTO Income { get; set; }
         public required ScheduleDTO Schedule { get; set; }
+        public required CompanyEmployeeTaxDTO CompanyEmployeeTax { get; set; }
     }
-    public record ScheduleIncomeSelect
+    public record EndMonthDataSelect
     {
         public Income? Income { get; set; }
         public Schedule? Schedule { get; set; }
+        public CompanyEmployeeTax? CompanyEmployeeTax { get; set; }
     }
 }

27e2ce6e786f0053da2ffc51c7cbc4d606dd3bd0
DamyanKolev <damyankolev2@gmail.com>
2024-05-06 17:12:26 +0300

    change style

--- reactapp/src/components/Forms/schedule/UpdateScheduleForm.tsx
+++ reactapp/src/components/Forms/schedule/UpdateScheduleForm.tsx
@@ -26,106 +26,114 @@ const UpdateScheduleForm: FC<Props> = ({getEditMode, getFormData, getFormState, 
     };
 
     return (
-        <FlexBox alignItems={FlexBoxAlignItems.End} direction={FlexBoxDirection.Column} style={{padding: "1rem 2rem", gap: ".5rem", width:"fit-content"}}>
-            <FlexBox alignItems={FlexBoxAlignItems.Center} style={{gap:"1rem"}}>
-                <Label>Дни в осигуряване</Label>
-                <StandardInputField
-                    editMode={getEditMode()}
-                    value={getFormData().insuranceDays.toString()}
-                    onInput={handleOnInput}
-                    name={"insuranceDays"}
-                    valueState={getFormState().insuranceDays.valueState}
-                    dataType={DataType.Int}
-                />
+        <FlexBox style={{gap:"4rem", padding:".3rem 2rem", width: "fit-content"}} direction={FlexBoxDirection.Column}>
+
+            <FlexBox alignItems={FlexBoxAlignItems.End} direction={FlexBoxDirection.Column} style={{gap:".5rem"}}>
+                <FlexBox alignItems={FlexBoxAlignItems.Center} style={{gap:"1rem"}}>
+                    <Label>Дни в осигуряване</Label>
+                    <StandardInputField
+                        editMode={getEditMode()}
+                        value={getFormData().insuranceDays.toString()}
+                        onInput={handleOnInput}
+                        name={"insuranceDays"}
+                        valueState={getFormState().insuranceDays.valueState}
+                        dataType={DataType.Int}
+                    />
+                </FlexBox>
+                <FlexBox alignItems={FlexBoxAlignItems.Center} style={{gap:"1rem"}}>
+                    <Label required>Обработени и други дни с осигурителни вноски</Label>
+                    <StandardInputField
+                        editMode={getEditMode()}
+                        value={getFormData().insuranceExperienceDays.toString()}
+                        onInput={handleOnInput}
+                        name={"insuranceExperienceDays"}
+                        valueState={getFormState().insuranceExperienceDays.valueState}
+                        dataType={DataType.Int}
+                    />
+                </FlexBox>
+                <FlexBox alignItems={FlexBoxAlignItems.Center} style={{gap:"1rem"}}>
+                    <Label required>Дни във временнна неработоспособност</Label>
+                    <StandardInputField
+                        editMode={getEditMode()}
+                        value={getFormData().incapacityDays.toString()}
+                        onInput={handleOnInput}
+                        name={"incapacityDays"}
+                        valueState={getFormState().incapacityDays.valueState}
+                        dataType={DataType.Int}
+                    />
+                </FlexBox>
+                <FlexBox alignItems={FlexBoxAlignItems.Center} style={{gap:"1rem"}}>
+                    <Label required>Дни за отглеждане на дете</Label>
+                    <StandardInputField
+                        editMode={getEditMode()}
+                        value={getFormData().childcareDays.toString()}
+                        onInput={handleOnInput}
+                        name={"childcareDays"}
+                        valueState={getFormState().childcareDays.valueState}
+                        dataType={DataType.Int}
+                    />
+                </FlexBox>
             </FlexBox>
-            <FlexBox alignItems={FlexBoxAlignItems.Center} style={{gap:"1rem"}}>
-                <Label required>Обработени и други дни с осигурителни вноски</Label>
-                <StandardInputField
-                    editMode={getEditMode()}
-                    value={getFormData().insuranceExperienceDays.toString()}
-                    onInput={handleOnInput}
-                    name={"insuranceExperienceDays"}
-                    valueState={getFormState().insuranceExperienceDays.valueState}
-                    dataType={DataType.Int}
-                />
+
+            
+            <FlexBox alignItems={FlexBoxAlignItems.End} direction={FlexBoxDirection.Column} style={{gap:".5rem"}}>
+                <FlexBox alignItems={FlexBoxAlignItems.Center} style={{gap:"1rem"}}>
+                    <Label required>Дни без осигурителени вноски, зачетени за осигурителен стаж</Label>
+                    <StandardInputField
+                        editMode={getEditMode()}
+                        value={getFormData().withoutInsuranceDays.toString()}
+                        onInput={handleOnInput}
+                        name={"withoutInsuranceDays"}
+                        valueState={getFormState().withoutInsuranceDays.valueState}
+                        dataType={DataType.Int}
+                    />
+                </FlexBox>
+                <FlexBox alignItems={FlexBoxAlignItems.Center} style={{gap:"1rem"}}>
+                    <Label required>Дни в неплатен отпуск зачетени за осигурителен стаж</Label>
+                    <StandardInputField
+                        editMode={getEditMode()}
+                        value={getFormData().unpaidLeaveDays.toString()}
+                        onInput={handleOnInput}
+                        name={"unpaidLeaveDays"}
+                        valueState={getFormState().unpaidLeaveDays.valueState}
+                        dataType={DataType.Int}
+                    />
+                </FlexBox>
+                <FlexBox alignItems={FlexBoxAlignItems.Center} style={{gap:"1rem"}}>
+                    <Label required>Дни във временна неработоспособност с възнаграждение от работодателя</Label>
+                    <StandardInputField
+                        editMode={getEditMode()}
+                        value={getFormData().paidIncapacityDays.toString()}
+                        onInput={handleOnInput}
+                        name={"paidIncapacityDays"}
+                        valueState={getFormState().paidIncapacityDays.valueState}
+                        dataType={DataType.Int}
+                    />
+                </FlexBox>
+                <FlexBox alignItems={FlexBoxAlignItems.Center} style={{gap:"1rem"}}>
+                    <Label required>Отработени часове</Label>
+                    <StandardInputField
+                        editMode={getEditMode()}
+                        value={getFormData().workedHours.toString()}
+                        onInput={handleOnInput}
+                        name={"workedHours"}
+                        valueState={getFormState().workedHours.valueState}
+                        dataType={DataType.Int}
+                    />
+                </FlexBox>
+                <FlexBox alignItems={FlexBoxAlignItems.Center} style={{gap:"1rem"}}>
+                    <Label required>Часове положен извънреден труд</Label>
+                    <StandardInputField
+                        editMode={getEditMode()}
+                        value={getFormData().overtimeHours.toString()}
+                        onInput={handleOnInput}
+                        name={"overtimeHours"}
+                        valueState={getFormState().overtimeHours.valueState}
+                        dataType={DataType.Int}
+                    />
+                </FlexBox>
             </FlexBox>
-            <FlexBox alignItems={FlexBoxAlignItems.Center} style={{gap:"1rem"}}>
-                <Label required>Дни във временнна неработоспособност</Label>
-                <StandardInputField
-                    editMode={getEditMode()}
-                    value={getFormData().incapacityDays.toString()}
-                    onInput={handleOnInput}
-                    name={"incapacityDays"}
-                    valueState={getFormState().incapacityDays.valueState}
-                    dataType={DataType.Int}
-                />
-            </FlexBox>
-            <FlexBox alignItems={FlexBoxAlignItems.Center} style={{gap:"1rem"}}>
-                <Label required>Дни за отглеждане на дете</Label>
-                <StandardInputField
-                    editMode={getEditMode()}
-                    value={getFormData().childcareDays.toString()}
-                    onInput={handleOnInput}
-                    name={"childcareDays"}
-                    valueState={getFormState().childcareDays.valueState}
-                    dataType={DataType.Int}
-                />
-            </FlexBox>
-            <FlexBox alignItems={FlexBoxAlignItems.Center} style={{gap:"1rem"}}>
-                <Label required>Дни без осигурителени вноски, зачетени за осигурителен стаж</Label>
-                <StandardInputField
-                    editMode={getEditMode()}
-                    value={getFormData().withoutInsuranceDays.toString()}
-                    onInput={handleOnInput}
-                    name={"withoutInsuranceDays"}
-                    valueState={getFormState().withoutInsuranceDays.valueState}
-                    dataType={DataType.Int}
-                />
-            </FlexBox>
-            <FlexBox alignItems={FlexBoxAlignItems.Center} style={{gap:"1rem"}}>
-                <Label required>Дни в неплатен отпуск зачетени за осигурителен стаж</Label>
-                <StandardInputField
-                    editMode={getEditMode()}
-                    value={getFormData().unpaidLeaveDays.toString()}
-                    onInput={handleOnInput}
-                    name={"unpaidLeaveDays"}
-                    valueState={getFormState().unpaidLeaveDays.valueState}
-                    dataType={DataType.Int}
-                />
-            </FlexBox>
-            <FlexBox alignItems={FlexBoxAlignItems.Center} style={{gap:"1rem"}}>
-                <Label required>Дни във временна неработоспособност с възнаграждение от работодателя</Label>
-                <StandardInputField
-                    editMode={getEditMode()}
-                    value={getFormData().paidIncapacityDays.toString()}
-                    onInput={handleOnInput}
-                    name={"paidIncapacityDays"}
-                    valueState={getFormState().paidIncapacityDays.valueState}
-                    dataType={DataType.Int}
-                />
-            </FlexBox>
-            <FlexBox alignItems={FlexBoxAlignItems.Center} style={{gap:"1rem"}}>
-                <Label required>Отработени часове</Label>
-                <StandardInputField
-                    editMode={getEditMode()}
-                    value={getFormData().workedHours.toString()}
-                    onInput={handleOnInput}
-                    name={"workedHours"}
-                    valueState={getFormState().workedHours.valueState}
-                    dataType={DataType.Int}
-                />
-            </FlexBox>
-            <FlexBox alignItems={FlexBoxAlignItems.Center} style={{gap:"1rem"}}>
-                <Label required>Часове положен извънреден труд</Label>
-                <StandardInputField
-                    editMode={getEditMode()}
-                    value={getFormData().overtimeHours.toString()}
-                    onInput={handleOnInput}
-                    name={"overtimeHours"}
-                    valueState={getFormState().overtimeHours.valueState}
-                    dataType={DataType.Int}
-                />
-            </FlexBox>
+            
         </FlexBox>
     );
 };

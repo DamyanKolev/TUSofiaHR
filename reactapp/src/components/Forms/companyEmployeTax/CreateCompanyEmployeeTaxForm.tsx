9be85e2be5704a00bdb9682a1d753995308a8126
DamyanKolev <damyankolev2@gmail.com>
2024-05-06 17:13:26 +0300

    fix creation form

--- reactapp/src/components/Forms/companyEmployeTax/CreateCompanyEmployeeTaxForm.tsx
+++ reactapp/src/components/Forms/companyEmployeTax/CreateCompanyEmployeeTaxForm.tsx
@@ -1,32 +1,32 @@
 import DataType from "@app-types/enums/DataType";
 import SmallTableSelect from "@components/Selects/TableSelect/SmallTableSelect";
 import { ChangeData } from "@models/EventData/ChangeData";
-import { CompanyEmployeeTax } from "@models/HR/declarations/CompanyEmployeeTax";
+import { CompanyEmployeeTaxInsert } from "@/models/HR/CompanyEmployeeTax";
 import { paymentTypeJoinTable } from "@models/JoinTableInfo/PaymentJoinTableInfo";
 import { DatePicker, DatePickerDomRef, FlexBox, FlexBoxAlignItems, FlexBoxDirection, Label, StandardListItemDomRef, Ui5CustomEvent } from "@ui5/webcomponents-react";
 import { DatePickerChangeEventDetail } from "@ui5/webcomponents/dist/DatePicker.js";
 import { largeFormItem } from "@utils/css";
-import { setDateToInputDefaultValue } from "@utils/forms/setInputDefaultValue";
 import { FC } from "react";
+import { CompanyEmployeeTaxFormState } from "@/models/States/companyEmployeeTax/CompanyEmployeeTaxFormState";
 
 
-interface CreateCompanyEmployeeTaxProps {
-    // getFormState: () => DepartmentFormState,
-    getFormData: () => CompanyEmployeeTax,
-    setFormStates: (changeData: ChangeData, currentKey: int) => void,
+interface Props {
+    getFormState: () => CompanyEmployeeTaxFormState,
+    getFormData: () => CompanyEmployeeTaxInsert,
+    setFormStates: (changeData: ChangeData) => void,
 }
 
 
-const CreateCompanyEmployeeTaxForm: FC<CreateCompanyEmployeeTaxProps> = ({getFormData, setFormStates}) => {
+const CreateCompanyEmployeeTaxForm: FC<Props> = ({getFormData, setFormStates}) => {
     const handleConfirm = (selectedItem: StandardListItemDomRef, name: string) => {
-        const value = selectedItem.children[0].children[0].textContent
+        const value = selectedItem.id
         if (value) {
             const changeData: ChangeData = {
                 value: value,
                 name: name,
                 valueType: DataType.Int,
             }
-            setFormStates(changeData, getFormData().employeeId)
+            setFormStates(changeData)
         }
     }
 
@@ -37,11 +37,11 @@ const CreateCompanyEmployeeTaxForm: FC<CreateCompanyEmployeeTaxProps> = ({getFor
             valueType: event.target.dataset.type,
             name: event.target.name,
         }
-        setFormStates(changeData, getFormData().employeeId)
+        setFormStates(changeData)
     }
 
     return (
-        <FlexBox alignItems={FlexBoxAlignItems.End} direction={FlexBoxDirection.Column} style={{gap:".5rem", marginLeft:"4rem"}}>
+        <FlexBox alignItems={FlexBoxAlignItems.End} direction={FlexBoxDirection.Column} style={{padding: "1rem 2rem", gap: ".5rem", width:"fit-content"}}>
             <FlexBox alignItems={FlexBoxAlignItems.Center} style={{gap:"1rem"}}>
                 <Label required>Здравно осигурителен доход за лицата по чл. 40</Label>
                 <SmallTableSelect
@@ -55,7 +55,7 @@ const CreateCompanyEmployeeTaxForm: FC<CreateCompanyEmployeeTaxProps> = ({getFor
                 <DatePicker
                     style={largeFormItem}
                     name="disbursementAccrualDate"
-                    value={setDateToInputDefaultValue(getFormData().disbursementAccrualDate)}
+                    value={getFormData().disbursementAccrualDate}
                     onChange={handleDateChange}
                     data-type={DataType.Date}
                 />
